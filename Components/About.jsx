6afaf39747121b6ad19c46974c32fa24ed950879
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-01 20:08:01 +0530

    s1

--- Components/About.jsx
+++ Components/About.jsx
@@ -11,24 +11,24 @@ const About = () => {
                     <h1>About Me</h1>
                     <div className="box">
                         <div className="left">
-                        <Image src={DPA} width="280" height="450" objectFit='cover' objectPosition="top" alt='arnabs image' className="img"/>
+                        <Image src={DPA} width="450" height="450" objectFit='cover' alt='arnabs image' className="img"/>
                         </div>
                         <div className="right">
                             <div className="cards">
                                 <div className="card">
                                     <BsFillAwardFill/>
                                     <p>Experience</p>
-                                    <h5>3+ Years Working</h5>
+                                    <h4>3+ Years Working</h4>
                                 </div>
                                 <div className="card">
                                     <FiFolder/>
                                     <p>Clients</p>
-                                    <h5>3200+ Worldwide</h5>
+                                    <h4>3200+ Worldwide</h4>
                                 </div>
                                 <div className="card">
                                     <FiUsers/>
                                     <p>Projects</p>
-                                    <h5>80+ Completed</h5>
+                                    <h4>80+ Completed</h4>
                                 </div>
                             </div>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente vero, aliquam error veritatis ducimus nisi, enim mollitia quaerat aspernatur praesentium nobis ab! Iste, voluptatum? Minus laboriosam possimus dolorem ab necessitatibus.</p>
