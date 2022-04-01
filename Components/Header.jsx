1fbae5b5562f59d7b2d67502569896872f31c4cd
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-01 11:31:10 +0530

    s1

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,32 @@
+import {BsGithub,BsLinkedin} from "react-icons/bs";
+import CV from '../public/assets/me.png'
+import DP from '../public/assets/me.png'
+import Image from 'next/image';
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <header id="header">
+                <h5>Hello I'm</h5>
+                <h1></h1>
+                <h6>Fullstack Developer & Blockchain Developer</h6>
+                <div className="cta">
+                    <a href={CV} download >Download CV</a>
+                    <a href="#contact">Let's Talk</a>
+                </div>
+                <div className="main">
+                    <div className="socials">
+                        <a href=""><BsGithub/></a>
+                        <a href=""><BsLinkedin/></a>
+                    </div>
+                    <div className="dp">
+                    <Image src={DP} width="300" height="500" objectFit='cover' alt='arnabs image' className="img"/>
+                    </div>
+                    <div className="scroll">
+                        <p>Scroll Down</p>
+                    </div>
+                </div>
+            </header>
+        </>
     );
 }
 
