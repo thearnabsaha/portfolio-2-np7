1172abc4239ba25669ae3e11de985b955542dc13
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-05 09:50:04 +0530

    s3

--- Components/Experience.jsx
+++ Components/Experience.jsx
@@ -1,7 +1,69 @@
 const Experience = () => {
     return (
         <>
-            
+            <div id="experience">
+                <div className="container">
+                    <h4>What Skills I Have</h4>
+                    <p>My Experience</p>
+                    <div className="box">
+                        <div className="left">
+                            <h2>Frontend Development</h2>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="right">
+                        <h2>Frontend Development</h2>
+                            <div className="smBox">
+                                <h2>%</h2>
+                                <div className="content">
+                                    <h1>HTML</h1>
+                                    <p>experienced</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
