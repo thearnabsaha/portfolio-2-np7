2be6b902a79bc95329f0ac50321a7aeef97c681e
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-07 15:35:00 +0530

    s2

--- Components/Portfolio.jsx
+++ Components/Portfolio.jsx
@@ -1,8 +1,8 @@
 const Portfolio = () => {
     return (
-        <div>
-            Portfolio
-        </div>
+        <>
+            portfolio
+        </>
     );
 }
 
--- Components/Services.jsx
+++ Components/Services.jsx
@@ -1,8 +1,77 @@
+import { AiOutlineCheckCircle } from "react-icons/ai";
 const Services = () => {
     return (
-        <div>
-            S
-        </div>
+        <>
+            <div id="services">
+                <div className="container">
+                    <h6>What I Offer</h6>
+                    <h5>Services</h5>
+                    <div className="bigBox">
+                        <div className="card">
+                            <h4>UI/UX Design</h4>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <h4>Web Development</h4>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <h4>Content Creation</h4>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="box">
+                                <AiOutlineCheckCircle className="icon"/>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
