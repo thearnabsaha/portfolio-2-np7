61eae585cc46c36f127f1dc953a27c93cbf58a06
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-07 12:03:29 +0530

    s2

--- Components/Experience.jsx
+++ Components/Experience.jsx
@@ -13,7 +13,73 @@ const Experience = () => {
                             <div className="smBox">
                                 <div className="contentContainer">
                                     <div className="content">
-                                        <h1> <AiOutlineCheck className="icon"/> HTML</h1>
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
+                                        <p>experienced</p>
+                                    </div>
+                                </div>
+                                <div className="contentContainer">
+                                    <div className="content">
+                                    <div className="head">
+                                        <AiOutlineCheck className="icon"/>
+                                        <h1>HTML</h1>
+                                    </div>
                                         <p>experienced</p>
                                     </div>
                                 </div>
