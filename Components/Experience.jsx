041ea76a764756ff3b9497c18ce5f1ca363b43f5
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-07 12:13:53 +0530

    s2

--- Components/Experience.jsx
+++ Components/Experience.jsx
@@ -3,7 +3,23 @@ import { AiOutlineCheck } from "react-icons/ai";
 const Experience = () => {
     return (
         <>
-            
+            <div id="experience">
+                <div className="container">
+                    <h6>What Skills I Have</h6>
+                    <h5>My Experience</h5>
+                    <div className="box">
+                        <h4>Frontend Development</h4>
+                        <div className="box1">
+                            <div className="right">
+
+                            </div>
+                            <div className="left">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
