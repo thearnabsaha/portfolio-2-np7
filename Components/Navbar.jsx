7c0f8bed4946a9600df69a2d6388d5f07f0bf239
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-08 20:00:07 +0530

    s2

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,8 +1,18 @@
+import {AiOutlineHome,AiOutlineUser,AiOutlineBook,AiOutlineMessage} from "react-icons/ai";
+import { RiServiceLine } from "react-icons/ri";
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <>
+            <div id="navbar">
+                <div className="container">
+                    <AiOutlineHome className="icon"/>
+                    <AiOutlineUser className="icon"/>
+                    <AiOutlineBook className="icon"/>
+                    <RiServiceLine className="icon"/>
+                    <AiOutlineMessage className="icon"/>
+                </div>
+            </div>
+        </>
     );
 }
 
