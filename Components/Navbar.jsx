fa4c3a6d4b93d12cbb5219aa34c8b46e309149ab
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-08 20:41:44 +0530

    s2

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,15 +1,18 @@
 import {AiOutlineHome,AiOutlineUser,AiOutlineBook,AiOutlineMessage} from "react-icons/ai";
 import { RiServiceLine } from "react-icons/ri";
+import { useState } from "react";
+useState
 const Navbar = () => {
+    const [first, setFirst] = useState("#")
     return (
         <>
             <div id="navbar">
                 <div className="container">
-                    <a href="#"><AiOutlineHome className="icon active"/></a>
-                    <a href="#about"><AiOutlineUser className="icon"/></a>
-                    <a href="#experience"><AiOutlineBook className="icon"/></a>
-                    <a href="#portfolio"><RiServiceLine className="icon"/></a>
-                    <a href="#contact"><AiOutlineMessage className="icon"/></a>
+                    <a href="#"><AiOutlineHome id="icon" className={first === "#" ? "active" : ""} onClick={()=>setFirst("#")} /></a>
+                    <a href="#about"><AiOutlineUser id="icon" className={first === "#about" ? "active" : ""} onClick={()=>setFirst("#about")} /></a>
+                    <a href="#experience"><AiOutlineBook id="icon" className={first === "#experience" ? "active" : ""} onClick={()=>setFirst("#experience")} /></a>
+                    <a href="#portfolio"><RiServiceLine id="icon" className={first === "#portfolio" ? "active" : ""} onClick={()=>setFirst("#portfoliio")} /></a>
+                    <a href="#contact"><AiOutlineMessage id="icon" className={first === "#contact" ? "active" : ""} onClick={()=>setFirst("#contact")} /></a>
                 </div>
             </div>
         </>
