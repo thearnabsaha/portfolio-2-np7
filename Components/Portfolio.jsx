e7397dd5991e746de7dad463761fc8f9a2a57b4f
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-08 12:50:25 +0530

    s3

--- Components/Portfolio.jsx
+++ Components/Portfolio.jsx
@@ -1,7 +1,22 @@
+import Image from 'next/image';
+import DPA from '../public/assets/me-about.jpg'
 const Portfolio = () => {
     return (
         <>
-            portfolio
+            <div id="portfolio">
+                <div className="container">
+                    <h6>My Recent Work</h6>
+                    <h5>Portfolio</h5>
+                    <div className="cards">
+                        <div className="card">
+                            <Image src={DPA} width="400" height="400" objectFit='cover' alt='arnabs image' className="img"/>
+                            <h4>Crypto Currency Dashboard & Financial Visualization</h4>
+                            <a href="" target="_blank">Github</a>
+                            <a href="" target="_blank">Live Demo</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
