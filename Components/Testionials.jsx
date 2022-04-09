4c026ddca2ba8ce107003934329b5ba5446c21da
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-09 11:44:21 +0530

    s3

--- Components/Testionials.jsx
+++ Components/Testionials.jsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
 const Testionials = () => {
     return (
-        <div>
-            Testimonials
-        </div>
+        <>
+            <div id="testimonials">
+                <div className="container">
+                    <h6>Review from clients</h6>
+                    <h5>Testimonials</h5>
+                    <div className="cards">
+                        <div className="card">
+                            <Image src={DPA} width="400" height="400" objectFit='cover' alt='arnabs image' className="img"/>
+                            <h4>Arindam saha</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga nisi! Iusto suscipit natus dignissimos magni quidem dolor ducimus cumque soluta itaque maxime eius mollitia nisi ad sint, eligendi quisquam.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
