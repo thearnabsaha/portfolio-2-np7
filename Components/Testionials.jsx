96cdb8e449af63f2c47092866524ae7e9f1eeb83
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-10 19:54:05 +0530

    s5

--- Components/Testionials.jsx
+++ Components/Testionials.jsx
@@ -3,6 +3,24 @@ import PA from '../public/assets/avatar1.jpg'
 import PB from '../public/assets/avatar2.jpg'
 import PC from '../public/assets/avatar3.jpg'
 import PD from '../public/assets/avatar4.jpg'
+
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
+
+
+
 const Testionials = () => {
     return (
         <>
@@ -10,28 +28,28 @@ const Testionials = () => {
                 <div className="container">
                     <h6>Review from clients</h6>
                     <h5>Testimonials</h5>
-                    <div className="cards">
-                        <div className="card">
+                    <Swiper navigation={true} modules={[Navigation]} className="mySwiper cards">
+                        <SwiperSlide className="card">
                             <Image src={PA} width="60" height="60" objectFit='cover' alt='arnabs image' className="img"/>
                             <h4>Arindam saha</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga nisi! Iusto suscipit natus dignissimos magni quidem dolor ducimus cumque soluta itaque maxime eius mollitia nisi ad sint, eligendi quisquam.</p>
-                        </div>
-                        <div className="card">
+                        </SwiperSlide>
+                        <SwiperSlide className="card">
                             <Image src={PB} width="60" height="60" objectFit='cover' alt='arnabs image' className="img"/>
                             <h4>Arindam saha</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga nisi! Iusto suscipit natus dignissimos magni quidem dolor ducimus cumque soluta itaque maxime eius mollitia nisi ad sint, eligendi quisquam.</p>
-                        </div>
-                        <div className="card">
+                        </SwiperSlide>
+                        <SwiperSlide className="card">
                             <Image src={PC} width="60" height="60" objectFit='cover' alt='arnabs image' className="img"/>
                             <h4>Arindam saha</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga nisi! Iusto suscipit natus dignissimos magni quidem dolor ducimus cumque soluta itaque maxime eius mollitia nisi ad sint, eligendi quisquam.</p>
-                        </div>
-                        <div className="card">
+                        </SwiperSlide>
+                        <SwiperSlide className="card">
                             <Image src={PD} width="60" height="60" objectFit='cover' alt='arnabs image' className="img"/>
                             <h4>Arindam saha</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga nisi! Iusto suscipit natus dignissimos magni quidem dolor ducimus cumque soluta itaque maxime eius mollitia nisi ad sint, eligendi quisquam.</p>
-                        </div>
-                    </div>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </>
