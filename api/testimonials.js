81ec4c61a15bdd681feb52fabbce55d3d8044b2e
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-12 11:43:24 +0530

    s5

---
+++ api/testimonials.js
@@ -0,0 +1,49 @@
+export const testimonials = [
+    {
+        id: 1,
+        title: "Crypto Currency Dashboard & Financial Visualization",
+        github: "https://github.com/thearnabsaha/web3.github.io",
+        demo:"https://tasmeal-github-io.vercel.app/",
+        icon:"/../public/assets/portfolio1.jpg",
+    },
+    {
+        id: 2,
+        title: "Crypto Currency Dashboard & Financial Visualization",
+        github: "https://github.com/thearnabsaha/web3.github.io",
+        demo:"https://tasmeal-github-io.vercel.app/",
+        icon:"/../public/assets/portfolio2.jpg",
+
+    },
+    {
+        id: 3,
+        title: "Crypto Currency Dashboard & Financial Visualization",
+        github: "https://github.com/thearnabsaha/web3.github.io",
+        demo:"https://tasmeal-github-io.vercel.app/",
+        icon:"/../public/assets/portfolio3.jpg",
+
+    },
+    {
+        id: 4,
+        title: "Crypto Currency Dashboard & Financial Visualization",
+        github: "https://github.com/thearnabsaha/web3.github.io",
+        demo:"https://tasmeal-github-io.vercel.app/",
+        icon:"/../public/assets/portfolio4.jpg",
+
+    },
+    {
+        id: 5,
+        title: "Crypto Currency Dashboard & Financial Visualization",
+        github: "https://github.com/thearnabsaha/web3.github.io",
+        demo:"https://tasmeal-github-io.vercel.app/",
+        icon:"/../public/assets/portfolio5.png",
+
+    },
+    {
+        id: 6,
+        title: "Crypto Currency Dashboard & Financial Visualization",
+        github: "https://github.com/thearnabsaha/web3.github.io",
+        demo:"https://tasmeal-github-io.vercel.app/",
+        icon:"/../public/assets/portfolio6.jpg",
+
+    },
+];
