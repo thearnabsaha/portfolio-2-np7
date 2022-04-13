ad1aa505e353274a21182b83ea8ce716cc7c57a9
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-13 10:36:21 +0530

    s5

--- Components/Testionials.jsx
+++ Components/Testionials.jsx
@@ -4,7 +4,7 @@ import {rec} from '../api/testimonials'
 import React from "react";
 const Testionials = () => {
     return (
-        <>
+        <div>
             <div id="testimonials">
                 <div className="container">
                     <h6>Review from clients</h6>
@@ -12,19 +12,19 @@ const Testionials = () => {
                     <div className="cards">
                     {
                         rec.map((e)=>{
-                            return <>
+                            return <div key={e.id}>
                                 <div className="card">
                                     <Image src={e.icon} width="60" height="60" objectFit='cover' alt='arnabs image' className="img"/>
                                     <h4>{e.title}</h4>
                                     <p>{e.desc}</p>
                                 </div>
-                            </>
+                            </div>
                         })
                     }
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
